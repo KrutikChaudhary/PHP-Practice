98b672745c10b733f6c8f7155e5c666e7ad4fcfa
KrutikChaudhary <krutikchaudhary39@gmail.com>
2023-11-21 12:26:35 -0400

    Replace add_to_shopping_cart.php

--- labs/lab6/add_to_shopping_cart.php
+++ labs/lab6/add_to_shopping_cart.php
@@ -1,36 +1,35 @@
 <?php
-include './arrays/arrays1.php';
 session_start();
+
+echo '<form method="post" action="add_to_cart.php">';
 
 if (isset($_POST['buy_button']) && isset($_POST['album_index'])) {
     $albumIndex = $_POST['album_index'];
 
-    // Check if the shopping cart session variable is set, if not, initialize it as an empty array
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = array();
     }
+
+    // Include arrays1.php to access $albums_data
+    include './music.php';
+
     $album = $albums_data[$albumIndex];
     $album['quantity'] = $_POST['quantity'];
+
     if (isset($_SESSION['cart'][$albumIndex])) {
         $_SESSION['cart'][$albumIndex]['quantity'] += $album['quantity'];
     } else {
         $_SESSION['cart'][$albumIndex] = $album;
     }
 
-
     $_SESSION['cart_count'] += $album['quantity'];
-    // Add the selected album to the shopping cart
     array_push($_SESSION['cart'], $albums_data[$albumIndex]);
+    $_SESSION['cart_count'] = count($_SESSION['cart']);
 
-    // Update the total number of items in the cart session variable
-    // $_SESSION['cart_count'] = count($_SESSION['cart']);
-
-    // Redirect back to the catalog page or any other page you want after adding to the cart
-    header("Location: index.php");
-
+    // Redirect to cart.php after adding to the cart
+    header("Location: cart.php");
     exit();
 } else {
-    // Handle invalid requests, redirect to an error page or display an error message
     header("Location: error.php");
     exit();
 }
